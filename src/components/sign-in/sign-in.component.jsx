fa98e95d7620458370d5ce130b712e45f3d48531
devzons <devzons@gmail.com>
2020-12-05 19:07:34 -0800

    Redux, reducers, actions

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,7 +47,7 @@ class SignIn extends Component {
           <FormInput type='password' name='password' value={this.state.password} handleChange={this.handleChange} label='password' required />
           <div className='buttons'>
             <CustomButton type='submit'>Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
               Sign In with Google
             </CustomButton>
           </div>
