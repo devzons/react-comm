2c176f7ae4b0561b1a7d31987e57a54fb4805b28
devzons <devzons@gmail.com>
2021-01-05 07:04:33 -0800

    update components and page with reselect

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
+import { selectCurrentUser } from './redux/user/user.selectors'
+import { createStructuredSelector } from 'reselect'
 
 class App extends React.Component {
   unsubscribeFromAuth = null
@@ -49,8 +51,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 })
 
 const mapDispatchToProps = (dispatch) => ({
