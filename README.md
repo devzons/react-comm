1a4e7531094022fde6df6f03327c131f00b003ac
devzons <devzons@gmail.com>
2021-01-05 18:30:37 -0800

    added checkout page, checkout-item component and udated cart to use selectors

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### Reference
+
+- UTF-8 Dingbats: https://www.w3schools.com/charsets/ref_utf_dingbats.asp
--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import './App.css'
+import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
-import Header from './components/header/header.component'
+import CheckoutPage from './pages/checkout/checkout.component'
 import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
@@ -43,6 +44,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
+            <Route exact path='/checkout' component={CheckoutPage} />
             <Route exact path='/signin' render={() => (this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />)} />
           </Switch>
         </div>
